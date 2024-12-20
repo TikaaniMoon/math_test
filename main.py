d97fae7889c27fcabd5fa46d8793a01260f40613
Tikaani <dara.stormy@gmail.com>
2024-12-20 14:21:36 +0200

    feat: add function math_quiz

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ def generate_math_question(a, b):
     num1 = random.randint(a, b)
     num2 = random.randint(a, b)
 
-    operator = random.choice(['+', '-', '*', '/'])
+    operation = random.choice(['+', '-', '*', '/'])
 
-    result = f'{num1} {operator} {num2}'
+    result = f'{num1} {operation} {num2}'
     return result
-
-print(generate_math_question(1, 300))
 
 def check_answer(question, answer):
     try:
@@ -18,3 +16,26 @@ def check_answer(question, answer):
        return answer == eval(question)
     except ValueError:
         return False
+
+def math_quiz(num_questions=5):
+    print("Добро пожаловать в математический тест!")
+    correct_answers = 0
+
+    for i in range(num_questions):
+        question = generate_math_question(1, 300)
+        answer = input(f'{question} = ')
+        if check_answer(question, answer):
+            correct_answers += 1
+
+    print("Тест завершен.")
+    print(f"Вы правильно решили {correct_answers} из {num_questions} вопросов.")
+
+    if correct_answers >= num_questions * 0.8:
+        print("Отлично! Вы получаете оценку A.")
+    elif correct_answers >= num_questions * 0.6:
+        print("Хорошо! Вы получаете оценку B.")
+    else:
+        print("Попробуйте еще раз. Вы получаете оценку C.")
+
+math_quiz(7)
+
